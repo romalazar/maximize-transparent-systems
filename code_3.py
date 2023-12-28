c222814802c692744770e57f00f376dc82a6ec01
Roman Lazarev <144658461+romalazar@users.noreply.github.com>
2023-12-28 19:09:38 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,13 @@
+        }
+
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+    public static void main(String[] args) {
+        for (int item : data) {
+    }
+public class RandomDataGenerator {
+
+            System.out.println("Random Number: " + item);
+        }
+}
